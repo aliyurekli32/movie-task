7d0a82c237d8921d21dc0b8780295c297fc726ef
aliyurekli32 <adamjesperblack@gmail.com>
2023-01-22 12:30:50 +0300

    add: card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,30 @@
+import Div from "../elements/Div"
+
+
+
+
+
+
 
 
 const Card = () => {
   return (
-    <div>Card</div>
+    <>
+      <Div >
+  <figure className="rltw-w" ><img className="rltw-w" src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">
+      Shoes!
+      <div className="badge badge-secondary">NEW</div>
+    </h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <div className="badge badge-outline">Fashion</div> 
+      <div className="badge badge-outline">Products</div>
+    </div>
+  </div>
+</Div>
+    </>
   )
 }
 
