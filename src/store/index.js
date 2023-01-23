ad212c86c076c212cafeb16d280cdd4671706533
aliyurekli32 <adamjesperblack@gmail.com>
2023-01-23 11:16:28 +0300

    update: store

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth-slice"
-
+import movieReducer from "./get-Movie"
 const store=configureStore({
     reducer:{
-        auth: authReducer
+        auth: authReducer,
+        movie: movieReducer,
     }
 })
 
