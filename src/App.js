6829400b31c9a6861e2d8a29780f39453c69a447
aliyurekli32 <adamjesperblack@gmail.com>
2023-01-25 17:31:59 +0300

    update: root directory

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 
 
+import { useEffect } from 'react';
 import './App.css';
 import AppRouter from './router/AppRouter';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const {data}=useSelector(state=>state)
+
+  useEffect(()=>{
+    window.localStorage.setItem("data",JSON.stringify(data))
+    console.log('data')
+  },[data])
+  
   return (<>
     <AppRouter/> 
     </>
--- src/index.js
+++ src/index.js
@@ -6,12 +6,13 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from "./store"
 import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <BrowserRouter>
-    <App />
+      <App />
     </BrowserRouter>
   </Provider>
 );
