182559f35cdfb4a14c8ad601ced8f79412840b11
aliyurekli32 <adamjesperblack@gmail.com>
2023-01-26 09:28:07 +0300

    update: film

--- src/components/FormFilm.jsx
+++ src/components/FormFilm.jsx
@@ -1,8 +1,12 @@
 import { useState } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { getMovie } from '../store/get-Movie'
+import Form from 'react-bootstrap/Form';
+import {Button} from 'react-bootstrap';
+import { addMovie } from '../store/movieData-Slice';
 
 const FormFilm = () => {
+  const {movie}=useSelector(state=>state)
    const [sFilm,setSfilm]=useState("")
    const dispatch=useDispatch()
    const handleFilm=(e)=>{
@@ -10,14 +14,32 @@ const FormFilm = () => {
      dispatch(getMovie(sFilm))
    } 
   return (
-    <div>
-        <form onSubmit={handleFilm} action="">
-            <label htmlFor="search-film">Search Film</label>
-            <input onChange={(e)=>setSfilm(e.target.value)} id='search-film' name='search-film' value={sFilm}  type="text"/>
-            <button type='submit'>Search Film</button>
-        </form>
-    </div>
+    
+
+
+<div  className="py-2 w-25">
+<Form onSubmit={handleFilm} >
+<Form.Label htmlFor="search-film">Search Film: </Form.Label>
+<Form.Control
+ name='search'
+  type="text"
+  id="search-film"
+  onChange={(e)=>setSfilm(e.target.value)} value={sFilm}
+
+  
+/>
+<Button className='btn btn-info ms-2 mt-2' type='submit'>Search Film</Button>
+<Button onClick={()=>dispatch(addMovie(movie.movie))} className='btn btn-success ms-2 mt-2' type='button'>Add Film</Button>
+</Form>
+</div>
+
+   
   )
 }
 
 export default FormFilm
+
+
+
+
+
