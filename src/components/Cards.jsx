84ba00d0c49058fe47728fae209e4697b2bc4b44
aliyurekli32 <adamjesperblack@gmail.com>
2023-01-26 09:27:51 +0300

    update: cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,26 +1,40 @@
 import { Col, Container, Row } from "react-bootstrap"
 import MCard from "./MCard"
 import { useSelector } from "react-redux"
+import Modals from "./Modal"
+import { useState } from "react"
 
 
 
 
 const Cards = () => {
   const {data}=useSelector(state => state)
+  const [modalShow, setModalShow] = useState(false);
+  const [modalData,setModalData]=useState({
+    Title: "",
+    imdbRating: "",
+    imdbID: "",
+  })
 
-  console.log(data)
+  console.log(modalData)
   return (
     <Container >
         <Row className="justify-content-center gap-2">
           {
             data.map((item)=>{
               return(
-                <MCard item={item}/>
+                <MCard setModalData={setModalData} setModalShow={setModalShow} item={item}/>
               )
             })
           }
           
-        </Row>   
+        </Row>
+        <Modals
+        modalData={modalData}
+        setModalData={setModalData}
+         show={modalShow}
+         onHide={() => setModalShow(false)}
+        />  
     </Container>
   )
 }
