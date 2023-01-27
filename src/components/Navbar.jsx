82d4344c6e0a8c0df18b5444e952d7c5eb31d0d3
aliyurekli32 <adamjesperblack@gmail.com>
2023-01-27 08:22:28 +0300

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,48 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Button } from "react-bootstrap";
 
-
-const Navbar = () => {
+const NavbarR = () => {
   return (
-    <div>
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
-        <Link to="/">Home</Link>
-    </div>
-  )
-}
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      bg="dark"
+      variant="dark"
+    >
+      <Container>
+        <Navbar.Brand>Ali YÜREKLİ MOVİE APP</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Link
+              className="m-1"
+              to="/"
+            >
+              <Button>Home</Button>
+            </Link>
+          </Nav>
+          <Nav>
+            <Link
+              className="m-1"
+              to="/register"
+            >
+              <Button>Register</Button>
+            </Link>
+            <Link
+              className="m-1"
+              to="/login"
+              eventkey={2}
+            >
+              <Button>Login</Button>
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
-export default Navbar
+export default NavbarR;
