1f6ee43000d5fd0e25540203800b04e364689bec
aliyurekli32 <adamjesperblack@gmail.com>
2023-01-27 08:21:34 +0300

    fix: update and delete

--- src/store/movieData-Slice.js
+++ src/store/movieData-Slice.js
@@ -1,20 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const movieDataSlice=createSlice({
-    name:"movieData",
-    initialState:[],
+    name:"data",
+    initialState: JSON.parse(window.localStorage.getItem('data')) || [],
     reducers:{
         addMovie: (state,action)=>{
+            
+            
             state.push(action.payload)
         },
         deleteMovie: (state,action)=>{
-            state=state.filter(item=>item.imdbId!==action.payload.imdbId);
+            
+            
+            return state.filter((item)=>item.imdbID !== action.payload.imdbID);
+            
             
         },
         updateMovie: (state,action)=>{
-            state.map((item)=>{
-                if(item.imdbId==action.payload.imdbId){
-                    return {...item, Tittle:action.payload.Title, imdbRating: action.payload.imdbRating}
+            console.log(action)
+        return  state.map((item)=>{
+                if(item.imdbID==action.payload.imdbID){
+                    return {...item, Title:action.payload.Title, imdbRating: action.payload.imdbRating}
+                }else{
+                    return item
                 }
             })
         },
