12678096bc27f3d5f7c78fd1c7a84abc79c95a51
aliyurekli32 <adamjesperblack@gmail.com>
2023-01-27 08:21:49 +0300

    update:

--- src/components/FormFilm.jsx
+++ src/components/FormFilm.jsx
@@ -4,6 +4,7 @@ import { getMovie } from '../store/get-Movie'
 import Form from 'react-bootstrap/Form';
 import {Button} from 'react-bootstrap';
 import { addMovie } from '../store/movieData-Slice';
+import SCard from './SCard';
 
 const FormFilm = () => {
   const {movie}=useSelector(state=>state)
@@ -12,14 +13,16 @@ const FormFilm = () => {
    const handleFilm=(e)=>{
     e.preventDefault()
      dispatch(getMovie(sFilm))
+     setSfilm("")
    } 
   return (
     
 
 
-<div  className="py-2 w-25">
-<Form onSubmit={handleFilm} >
-<Form.Label htmlFor="search-film">Search Film: </Form.Label>
+<div  className="d-flex w-100  justify-content-evenly align-items-center">
+  <div className='w-50 '>
+    <Form onSubmit={handleFilm} >
+<Form.Label htmlFor="search-film">Search Film  </Form.Label>
 <Form.Control
  name='search'
   type="text"
@@ -28,9 +31,12 @@ const FormFilm = () => {
 
   
 />
+
 <Button className='btn btn-info ms-2 mt-2' type='submit'>Search Film</Button>
 <Button onClick={()=>dispatch(addMovie(movie.movie))} className='btn btn-success ms-2 mt-2' type='button'>Add Film</Button>
-</Form>
+</Form></div>
+<div className='w-25 '>{Object.keys(movie.movie).length > 0 && <SCard item={movie.movie} />} </div>
+
 </div>
 
    
