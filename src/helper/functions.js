3a02e47bedfa02b29f4acb17c1cf76d5e36c3506
aliyurekli32 <adamjesperblack@gmail.com>
2023-01-30 13:42:32 +0300

    update func.

--- src/helper/functions.js
+++ src/helper/functions.js
@@ -31,3 +31,20 @@ export const uniqYear=(data)=>{
     )
   return arrUniq
 }
+
+export const filterMovie =(filter,data)=>{
+  console.log(filter)
+  console.log(data)
+  const data1 = data.filter((item)=> filter.Year ? item.Year==filter.Year : item)
+  const data2 = data1.filter((item)=> filter.imdbRating ? Math.floor(Number(item.imdbRating))==filter.imdbRating : item)
+  const data3 = data2.filter((item)=> filter.Title ? item.Title==filter.Title : item)
+  const data4 = data3.filter((item)=> {
+    if(item.Genre.includes(filter.Genre)){
+      return item
+    }else{
+      return
+    }
+
+  })
+  return data4
+}
