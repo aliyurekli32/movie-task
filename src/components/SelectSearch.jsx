245e865ea66260d5860db2a98dd66074acf6f8f3
aliyurekli32 <adamjesperblack@gmail.com>
2023-01-30 12:10:47 +0300

    update: selectsearch comp.

--- src/components/SelectSearch.jsx
+++ src/components/SelectSearch.jsx
@@ -1,33 +1,31 @@
 import Form from 'react-bootstrap/Form';
 
 const SearchSelect=({uniqGenre,uniqYears})=> {
-  console.log(uniqGenre)
-  console.log(uniqYears)
   return (<>
     <Form.Select className='my-2' aria-label="Search Select">
       {
         uniqGenre && 
         <>
       <option>Choose Genre</option>
-      {uniqGenre?.map((item)=>{
+      {uniqGenre?.map((item,index)=>{
         return(
-          <>
-          <option value={item}>{item}</option>
-          </>
+         
+          <option key={index} value={item}>{item}</option>
+         
         )
       })}
       </>
       }
-      
+
       {
         uniqYears && 
         <>
       <option>Choose Year</option>
-      {uniqYears?.map((item)=>{
+      {uniqYears?.map((item,index)=>{
         return(
-          <>
-          <option value={item}>{item}</option>
-          </>
+        
+          <option key={index} value={item}>{item}</option>
+          
         )
       })}
       </>
