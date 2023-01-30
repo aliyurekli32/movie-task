b184dc0d18e35e1b21d2d87f335d32ef36408ce8
aliyurekli32 <adamjesperblack@gmail.com>
2023-01-30 12:09:33 +0300

    update: inputsearch comp.

--- src/components/InputSearch.jsx
+++ src/components/InputSearch.jsx
@@ -1,15 +1,27 @@
 import Form from 'react-bootstrap/Form';
 
-function InputSearch() {
+function InputSearch({filterData,setFilterData}) {
+  
+
+  const handleNumber=(e)=>{
+      if(e.target.value > 10){
+        setFilterData({...filterData,imdbRating: "10"})
+      }else if(e.target.value < 0){
+        setFilterData({...filterData,imdbRating: "0"})
+      }else{
+        setFilterData({...filterData,imdbRating:e.target.value})
+      }
+      
+  }
   return (
     <Form>
       <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
         <Form.Label>Search Movie DB</Form.Label>
-        <Form.Control type="text" placeholder="ice age" />
+        <Form.Control value={filterData.Title} onChange={(e)=>setFilterData({...filterData,Title: e.target.value})} type="text" placeholder="ice age" />
       </Form.Group>
       <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
         <Form.Label>Search IMDB Rating </Form.Label>
-        <Form.Control type="text" placeholder="6" />
+        <Form.Control onKeyDown={(evt) => ["e", "E", "+", "-"].includes(evt.key) && evt.preventDefault()} value={filterData.imdbRating} onChange={(e)=>{handleNumber(e)}} min={0} max={10} type="number" placeholder="6" />
       </Form.Group>
     </Form>
   );
