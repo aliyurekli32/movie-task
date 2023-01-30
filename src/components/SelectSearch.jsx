8b22060c558750ab1fc44d580f82673bf75923c5
aliyurekli32 <adamjesperblack@gmail.com>
2023-01-30 13:42:13 +0300

    update: Select

--- src/components/SelectSearch.jsx
+++ src/components/SelectSearch.jsx
@@ -1,12 +1,19 @@
 import Form from 'react-bootstrap/Form';
 
-const SearchSelect=({uniqGenre,uniqYears})=> {
+const SearchSelect=({uniqGenre,uniqYears,filterData,setFilterData})=> {
+  const handleSelect=(e)=>{
+    if(uniqGenre){
+      setFilterData({...filterData,Genre: e.target.value})
+    }else if(uniqYears){
+      setFilterData({...filterData,Year: e.target.value})
+    }
+  }
   return (<>
-    <Form.Select className='my-2' aria-label="Search Select">
+    <Form.Select onChange={(e)=>handleSelect(e)}  className='my-2' aria-label="Search Select">
       {
         uniqGenre && 
         <>
-      <option>Choose Genre</option>
+      <option value="">Choose Genre</option>
       {uniqGenre?.map((item,index)=>{
         return(
          
@@ -20,7 +27,7 @@ const SearchSelect=({uniqGenre,uniqYears})=> {
       {
         uniqYears && 
         <>
-      <option>Choose Year</option>
+      <option value="">Choose Year</option>
       {uniqYears?.map((item,index)=>{
         return(
         
