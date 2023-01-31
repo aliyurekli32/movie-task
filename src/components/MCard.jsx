a784f42cceb7ee86324b449ef6d929a06cbd1be2
aliyurekli32 <adamjesperblack@gmail.com>
2023-01-31 11:58:52 +0300

    update: mcard

--- src/components/MCard.jsx
+++ src/components/MCard.jsx
@@ -1,7 +1,8 @@
 import {Button,Card} from 'react-bootstrap';
 import { BsStarFill,BsStarHalf } from 'react-icons/bs';
-import { deleteMovie } from '../store/movieData-Slice';
-import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import DeleteModal from './DeleteModal';
+import { useState } from 'react';
 
 
 
@@ -10,18 +11,24 @@ import { useDispatch } from 'react-redux';
 
 
 const MCard = ({item,setModalShow,setModalData}) => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
  const {Title,Poster,imdbRating,Year,Genre,Plot,imdbID}=item
-const dispatch=useDispatch()
+
 
 
 const arr_Rating=Array.from({length: Math.floor(Number(imdbRating))}, (_, i) => i + 1)
 
  const rest_Rating=Array.from({length: (10-(Number(imdbRating) % 1 === 0 ? arr_Rating.length : !(isNaN(Number(imdbRating) % 1)) ? arr_Rating.length+1  : 0 ))}, (_, i) => i + 1)
   return (
-    <>
-     <Card 
-       className='h-100 col-sm-12 col-md-4 col-lg-3'>
-      <Card.Img className='card-image' variant="top" src={Poster==="N/A" ? "https://cdn.pixabay.com/photo/2016/04/14/07/50/film-1328405_960_720.jpg" : Poster} />
+    
+     <Card
+       className='h-100 col-sm-12 col-md-4 col-lg-3 link'>
+        <Link to={`/details/${imdbID}`} state={{item}}>
+        <Card.Img className='card-image' variant="top" src={Poster==="N/A" ? "https://cdn.pixabay.com/photo/2016/04/14/07/50/film-1328405_960_720.jpg" : Poster} />
+        </Link>
+      
       
       <Card.Body >
       <div>
@@ -49,14 +56,17 @@ const arr_Rating=Array.from({length: Math.floor(Number(imdbRating))}, (_, i) => 
         
         <div className='d-flex justify-content-center gap-1' >
         <Button  onClick={() => {setModalShow(true);setModalData({Title,imdbRating,imdbID})}} variant="primary">Update</Button>
-        <Button onClick={()=>dispatch(deleteMovie(item))} variant="danger">Delete</Button>
+        <Button onClick={()=>{handleShow()}} variant="danger">Delete</Button>
         </div>
         
       </Card.Body>
+      <DeleteModal show={show} handleClose={handleClose} item={item.imdbID}/>
     </Card>
 
 
-    </>
+   
+  
+   
   )
 }
 
