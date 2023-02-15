595f0928f905192bb992241f17375d2d98fe6836
aliyurekli32 <adamjesperblack@gmail.com>
2023-02-15 11:06:01 +0300

    update:

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import { useEffect } from 'react';
 import './App.css';
 import AppRouter from './router/AppRouter';
 import { useSelector } from 'react-redux';
+import useAuth from './helper/zustand';
 
 function App() {
   const {data,auth}=useSelector(state=>state)
+  const {user,authZus} = useAuth(state=>state)
 
   useEffect(()=>{
     window.localStorage.setItem("data",JSON.stringify(data))
     window.localStorage.setItem("auth",JSON.stringify(auth))
+    window.localStorage.setItem("user",JSON.stringify(user))
+    window.localStorage.setItem("authZus",JSON.stringify(authZus))
     
    
-  },[data,auth])
+  },[data,auth,user,authZus])
 
 
   return (<>
