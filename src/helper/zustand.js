48cf2ac6e2cf2382e3f43f20fd34570eeb10980c
aliyurekli32 <adamjesperblack@gmail.com>
2023-02-15 11:08:02 +0300

    update:

--- src/helper/zustand.js
+++ src/helper/zustand.js
@@ -1,8 +1,11 @@
 import { create } from 'zustand'
 
 const useAuth = create((set) => ({
-  authZus: false,
+  user: JSON.parse(window.localStorage.getItem('user')) || {},
+  authZus: JSON.parse(window.localStorage.getItem('authZus')) || false,
   setAuth: () => set((state) => ({ authZus: true })),
   removeAuth: () => set({ authZus: false }),
+  setUser: (data) => set((state)=>({user: data})),
+  removeUser: ()=> set({user:{}}),
 }))
 export default useAuth;
