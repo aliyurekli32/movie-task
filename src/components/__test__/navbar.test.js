b497608ad5081b916fe095572f41b85329a39a58
aliyurekli32 <adamjesperblack@gmail.com>
2023-02-16 17:03:42 +0300

    update:

--- src/components/__test__/navbar.test.js
+++ src/components/__test__/navbar.test.js
@@ -2,8 +2,11 @@ import {render, screen} from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import NavbarR from "../Navbar";
 import userEvent from "@testing-library/user-event";
+import useAuth from "../../helper/zustand";
 
 const MockFilmNavbar =({title})=>{
+    const user=useAuth(state=>state.user)
+    console.log(user)
     return(
         <BrowserRouter>
         <NavbarR title={title}/>
@@ -12,28 +15,45 @@ const MockFilmNavbar =({title})=>{
 
 }
 
-describe("go to wanted page",()=>{
-    it("it should go to desired page", ()=>{
-        render(<MockFilmNavbar/>);
-        const buttonElHome = screen.getByText(/Home/i);
-        expect(buttonElHome).toBeInTheDocument();
-    });
-    it("it should render Home", ()=>{
-        render(<MockFilmNavbar/>);
-        const buttonElHome = screen.getByRole("button",{name:"Home"});
-        expect(buttonElHome).toBeEnabled();
-    });
-    it("it should render Login", ()=>{
-        render(<MockFilmNavbar/>);
-        const buttonElLogin = screen.getByRole("button",{name:"Login"});
-        userEvent.click(buttonElLogin);
-        expect(window.location.href).toBe("http://localhost/login")
-    });
-    it("it should render Register", ()=>{
-        render(<MockFilmNavbar/>);
-        const buttonElRegister = screen.getByRole("button",{name:"Register"});
-        userEvent.click(buttonElRegister);
-        expect(window.location.href).toBe("http://localhost/register")
-    });
 
+describe("find user auth", ()=>{
+    it("should find user",()=>{
+       const mock = render(<MockFilmNavbar/>);
+        console.log(mock)
+        expect(user).toBe(undefined)
+    })
+  
 })
+
+
+// describe("go to wanted page",()=>{
+//     it("it should go to desired page", ()=>{
+//         render(<MockFilmNavbar/>);
+//         const buttonElHome = screen.getByText(/Home/i);
+//         expect(buttonElHome).toBeInTheDocument();
+//     });
+//     it("it should render Home", ()=>{
+//         render(<MockFilmNavbar/>);
+//         const buttonElHome = screen.getByRole("button",{name:"Home"});
+//         expect(buttonElHome).toBeEnabled();
+//     });
+//     it("it should render Login", ()=>{
+//         render(<MockFilmNavbar/>);
+//         const buttonElLogin = screen.getByRole("button",{name:"Login"});
+//         userEvent.click(buttonElLogin);
+//         expect(window.location.href).toBe("http://localhost/login")
+//     });
+//     it("it should render Register", ()=>{
+//         render(<MockFilmNavbar/>);
+//         const buttonElRegister = screen.getByRole("button",{name:"Register"});
+//         userEvent.click(buttonElRegister);
+//         expect(window.location.href).toBe("http://localhost/register")
+//     });
+
+    // it("it should render Home", ()=>{
+    //     render(<MockFilmNavbar/>);
+    //     const buttonElHome = screen.queryByRole("button",{name:"Logout"});
+    //     expect(buttonElHome).toBeNull()
+    // });
+
+// });
